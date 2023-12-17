68320613c9fc70759fa61010dd0e5e53a9081d8a
Saran Pariyar <spariyar05@gmail.com>
2023-12-17 08:47:51 +0545

    Added animation to navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,22 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="
-    ">Hello World</div>
+    ">Hello World
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+
+    </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,8 @@ import { useState } from "react"
 
 const Navbar = () => {
 const [toggleNav, setToggleNav] = useState<boolean>(false)
+const [isLoggedIn, setIsLoggedIn] = useState<string>("not_authenticated")
+
     const showNav = () =>{
         if(toggleNav){
             setToggleNav(false)
@@ -21,17 +23,18 @@ const [toggleNav, setToggleNav] = useState<boolean>(false)
     <h1 className='nav-logo'>DevCALT</h1>
     <div className="ham-icon" onClick={showNav}>X</div>
   </div>
-
-  <ul className={`nav-list ${toggleNav ? "show-hide-nav" : " "}`}>
+<div className={`nav-link-section ${toggleNav ? "show-hide-nav" : " "}`}>
+  <ul className="nav-list">
     <li><a href="#" className='nav-link'>Home</a></li>
     <li><a href="#" className='nav-link'>About</a></li>
     <li><a href="#" className='nav-link'>Why Join us?</a></li>
     <li><a href="#" className='nav-link'>Contact</a></li>
   </ul>
 
-  <div className={`action-buttons-section ${toggleNav ? "show-hide-nav" : " "}`}>
+  <div className="action-buttons-section">
     <a href="#" className="signup-btn auth-btn">Sign Up</a>
     <a href="#" className="login-btn auth-btn">Login</a>
+  </div>
   </div>
 </nav>
   )
