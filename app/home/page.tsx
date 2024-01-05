2e074b3f57fcef8c4495bd130b558c53a139f0ec
Saran Pariyar <spariyar05@gmail.com>
2024-01-05 10:31:06 +0545

    Hero section work & ShadCN

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./home.scss"
+import { Button } from "@/components/ui/button"
+
 const HomeComponent = () => {
   return (
     <section className='mx-8 my-8 text-center'>
         <h1 className='text-4xl font-bold'>Join DevCALT and find your fellow dev!</h1>
+        <p>Need a coding partner?</p>
+        <div>
+        <Button>View Devs list!</Button>
+        <p>
+        Want to create a full stack project but only know frontend or backend?
+        </p>
+        <p>Learn faster with a partner!</p>
+        </div>
+
     </section>
   )
 }
