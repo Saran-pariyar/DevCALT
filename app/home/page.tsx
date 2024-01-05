dd8dfe622540f581c8c5a83c32f3006ca8a22c5f
Saran Pariyar <spariyar05@gmail.com>
2024-01-05 10:06:35 +0545

    Added shadcn and start home section

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./home.scss"
+const HomeComponent = () => {
+  return (
+    <section className='mx-8 my-8 text-center'>
+        <h1 className='text-4xl font-bold'>Join DevCALT and find your fellow dev!</h1>
+    </section>
+  )
+}
+
+export default HomeComponent
