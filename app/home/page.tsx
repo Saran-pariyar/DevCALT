651cc78d6578acdb4dd5168d27fb419378a30d30
Saran Pariyar <spariyar05@gmail.com>
2024-01-12 22:20:05 +0545

    Made username states with input for user-auth

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,14 +5,28 @@ import { Button } from "@/components/ui/button"
 import { logIn, logOut } from '@/redux/features/auth-slice'
 import { useDispatch } from "react-redux"
 import { AppDispatch } from "@/redux/store"
+import { useState } from "react"
 
 const HomeComponent = () => {
+
+const [Username, setUsername] = useState<string>("")
 
 const dispatch = useDispatch<AppDispatch>();
 
   return (
     <section className='mx-8 my-8 text-center'>
-        <h1 className='text-4xl font-bold'>Join DevCALT and find your fellow dev!</h1>
+
+    <input type="text" onChange={(e)=>setUsername(e.target.value)} />
+<br />
+<button>Log In</button>
+<br /><br />
+<button>Log Out</button>
+
+
+
+
+
+        {/* <h1 className='text-4xl font-bold'>Join DevCALT and find your fellow dev!</h1>
         <p>Need a coding partner?</p>
         <div>
         <Button>View Devs list!</Button>
@@ -20,7 +34,7 @@ const dispatch = useDispatch<AppDispatch>();
         Want to create a full stack project but only know frontend or backend?
         </p>
         <p>Learn faster with a partner!</p>
-        </div>
+        </div> */}
 
     </section>
   )
