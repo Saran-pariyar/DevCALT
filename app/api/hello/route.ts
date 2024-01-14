2f8668ce88fae4c3b0d1c94d067dfdc17bc4c211
Saran Pariyar <spariyar05@gmail.com>
2024-01-14 22:16:46 +0545

    Quick api setup

---
+++ app/api/hello/route.ts
@@ -0,0 +1,32 @@
+type object_type = {
+    Name: String,
+    Age: Number
+}
+
+const obj : object_type = {Name: "Saran", Age: 20}
+
+export async function GET(request: Request) {
+    return new Response(`{
+        "data": {
+          "Name": "Saran",
+          "Age": 20,
+          "Address": {
+            "Street": "123 Main St",
+            "City": "Anytown",
+            "State": "CA",
+            "ZipCode": "12345"
+          },
+          "Contacts": [
+            {
+              "Type": "Email",
+              "Value": "saran@example.com"
+            },
+            {
+              "Type": "Phone",
+              "Value": "+1 (555) 123-4567"
+            }
+          ],
+          "Hobbies": ["Reading", "Coding", "Traveling"]
+        }
+      }`)
+  }
