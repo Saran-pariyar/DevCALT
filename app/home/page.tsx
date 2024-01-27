a58be7cf99b44d31e5b23522fe58f88c3db82d58
Saran Pariyar <spariyar05@gmail.com>
2024-01-27 13:05:46 +0545

    Working on hero section, all tailwind work

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -12,12 +12,12 @@ const HomeComponent = () => {
 
 
 
-<div className="hero-container">
-        <h1 className='text-5xl font-bold'>Join DevCALT and find your fellow dev!</h1>
-        <p className="text-3xl">Need a coding partner?</p>
-        <div className="flex items-center justify-between w-full my-5 text-3xl">
-        <Button className="text-3xl hover:text-black"><Link href={"/developers_list"} className="px-5"> View Devs list! </Link></Button>
-        <p className="text-3xl">
+<div className="hero-container border py-4">
+        <h1 className='text-5xl font-bold py-4'>Join DevCALT and find your fellow dev!</h1>
+        <p className="text-3xl my-9">Need a coding partner?</p>
+        <div className="flex items-center justify-between w-full text-3xl my-9 py-5">
+        <Link href={"/developers_list"} className="p-4  text-xl font-bold border rounded-xl transition-all duration-300 hover:bg-white hover:text-black"> View Devs list! </Link>
+        <p className="text-3xl py-5 w-4/6">
         Want to create a full stack project but only know frontend or backend?
         </p>
         </div>
