454bc34142f9dec259f990c0ca6141f51de74585
Saran Pariyar <spariyar05@gmail.com>
2024-01-28 19:59:34 +0545

    Removed all tailwindcss, using sass from now on

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,24 +8,24 @@ const HomeComponent = () => {
 
 
   return (
-    <section className='mx-8 my-8'>
+    <section className='hero-section'>
 
 
 
 <div className="hero-container py-4">
-        <h1 className='text-5xl font-bold py-4'>Join DevCALT and find your fellow dev!</h1>
-        <p className="text-3xl my-9">Need a coding partner?</p>
-        <div className="flex items-center flex-wrap justify-between w-full text-3xl my-9 py-5">
-        <Link href={"/developers_list"} className="p-4  text-xl font-bold border rounded-xl transition-all duration-300 hover:bg-white hover:text-black"> View Devs list! </Link>
-        <p className="text-3xl py-5 w-4/6">
+        <h1 className='hero-title'>Join DevCALT and find your fellow dev!</h1>
+        <p className="">Need a coding partner?</p>
+        <div className="">
+        <Link href={"/developers_list"} className=""> View Devs list! </Link>
+        <p className="">
         Want to create a full stack project but only know frontend or backend?
         </p>
         </div>
-        <p className="text-3xl">Learn faster with a partner!</p>
+        <p className="">Learn faster with a partner!</p>
         </div>
 
 <div className="about-us-section">
-  <h2 className="text-center text-4xl font-bold my-5">About Us</h2>
+  <h2 className="about-us-title">About Us</h2>
   <p className="about-text">
   The main goal of DevCALT is to connect developers with each other who are looking for a partner to collaborate with so they can create things that they imagined without having knowledge barrier. <br /><br />
 
