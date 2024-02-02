ed8d38f6219c87de57c4ea2d0a3216b8449ca2c7
Saran Pariyar <spariyar05@gmail.com>
2024-02-02 22:12:25 +0545

    Finished frontend of hero section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,14 @@
- "use client"
+//  "use client"
 
 import "./home.scss"
 // import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import Image from 'next/image'
-import connectMongoDB from "@/libs/mongodb"
+// import connectMongoDB from "@/libs/mongodb"
 
 const HomeComponent = async () => {
 
-  await connectMongoDB()
+  // await connectMongoDB()
   // console.log(connectMongoDB);
 
   return (
@@ -17,10 +17,18 @@ const HomeComponent = async () => {
 
 
       <div className="hero-container ">
-        <div className="hero-img" >
+        <div className="hero-text-container">
+          <h1 className="hero-title">Welcome To DevCALT!</h1>
+          <h2 className="hero-second-title"> Whether you want to create a full-stack project but only know frontend or backend or you want to learn to work with a team, we'll find a coding buddy for you.</h2>
+          <div className="hero-btn-section">
+            <Link href={"/developers_list"} className="hero-btn hero-btn-find">Find a partner</Link>
+            <Link href={"/developers_list"} className="hero-btn hero-btn-about">About Us</Link>
+          </div>
+        </div>
+        <div className="hero-img-container" >
 
         
-        <img src="/img/hero-bg.png" alt="" />
+        <img src="/img/hero-bg.png" alt="" className="hero-img" />
     </div>
     
       </div>
