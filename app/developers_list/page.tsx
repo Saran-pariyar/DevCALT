ba380a1a4187ab90206630883a5ef652d3fdd96b
Saran Pariyar <spariyar05@gmail.com>
2024-03-17 21:15:17 +0545

    metadata for devreq and developers_list

--- app/developers_list/page.tsx
+++ app/developers_list/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import type { Metadata } from 'next'
  
 export const metadata: Metadata = {
-  title: 'dev list',
-  description: '...',
+  title: 'DevCALT | dev list',
+  description: 'This is a dev list of Devcalt who listed themselves as looking for a partner with their requirements',
 }
 const page = () => {
   return (
--- app/devreq/page.tsx
+++ app/devreq/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'DevCALT | Create DevReq',
+  description: 'Create a Devreq in Devcalt to list yourself as searching for a partner',
+}
 
 const page = () => {
   return (
