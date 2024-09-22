e2c548ddf622ac95ccb82eb58bc90b80f03904fb
Saran Pariyar <spariyar05@gmail.com>
2024-09-22 19:56:31 +0545

    Fix vercel deployment error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,43 +1,43 @@
-"use client"
+// "use client"
 
-import { useAppSelector } from '@/redux/store'
-import React from 'react'
+// import { useAppSelector } from '@/redux/store'
+// import React from 'react'
 
-import { logOut, toggleModerator } from '@/redux/features/auth-slice'
-import { useDispatch } from "react-redux"
-import { AppDispatch } from "@/redux/store"
-import { useRouter } from "next/navigation"
-
-
-const DashBoard = () => {
-
-    const username = useAppSelector((state) => state.authReducer.value.username )
-    const isModerator = useAppSelector((state) => state.authReducer.value.isModerator)
+// import { logOut, toggleModerator } from '@/redux/features/auth-slice'
+// import { useDispatch } from "react-redux"
+// import { AppDispatch } from "@/redux/store"
+// import { useRouter } from "next/navigation"
 
 
-    const dispatch = useDispatch<AppDispatch>();
+// const DashBoard = () => {
 
-    const router = useRouter()
+//     const username = useAppSelector((state) => state.authReducer.value.username )
+//     const isModerator = useAppSelector((state) => state.authReducer.value.isModerator)
 
 
-    const onClickLogOut = () =>{
-      dispatch(logOut())
-      router.push("/")
+//     const dispatch = useDispatch<AppDispatch>();
 
-    }
+//     const router = useRouter()
 
-    const onClickToggleModerator = () =>{
-      dispatch(toggleModerator())
-    }
 
-  return (
-    <div className='flex flex-col items-center'>
-        <h1 className='text-white'>Username: {username}</h1>
-        <h2>{isModerator && "User is a Moderator"}</h2>
-        <button onClick={onClickLogOut}>log out</button>
-        <button onClick={onClickToggleModerator}>Toggle Moderator Status</button>
-    </div>
-  )
-}
+//     const onClickLogOut = () =>{
+//       dispatch(logOut())
+//       router.push("/")
 
-export default DashBoard
+//     }
+
+//     const onClickToggleModerator = () =>{
+//       dispatch(toggleModerator())
+//     }
+
+//   return (
+//     <div className='flex flex-col items-center'>
+//         <h1 className='text-white'>Username: {username}</h1>
+//         <h2>{isModerator && "User is a Moderator"}</h2>
+//         <button onClick={onClickLogOut}>log out</button>
+//         <button onClick={onClickToggleModerator}>Toggle Moderator Status</button>
+//     </div>
+//   )
+// }
+
+// export default DashBoard
