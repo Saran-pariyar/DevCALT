4db3a9f2458ab17b1e4b3e70d78f132f204a34d1
Saran Pariyar <spariyar05@gmail.com>
2024-09-25 22:20:00 +0545

    vercel deployment error fix

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,60 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-const SignUp = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission here, like sending data to backend
-    console.log('Form Data:', formData);
-  };
-
+const SignIn = () => {
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
-  );
-};
+    <div>SignIn</div>
+  )
+}
 
-export default SignUp;
+export default SignIn
