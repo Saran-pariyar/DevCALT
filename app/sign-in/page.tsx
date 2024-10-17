bae608bb7157a07b57985093f020fc066ca3b7c2
Saran Pariyar <spariyar05@gmail.com>
2024-10-17 22:45:39 +0545

    Template for signin

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -4,13 +4,33 @@ import { Input } from '@/components/ui/input'
 
 const SignIn = () => {
   return (
-    <div>
-<Input />
+    <section className='sign-in-form-section form-section'>
+      {/* <Input /> */}
+      <form action="" className="sign-in-form">
+        <div className="form-container">
+          <div className="logo-and-text-section">
+            <h1 className="logo">DevCALT</h1>
+            <h3 className=''>Glad to have you back!</h3>
+            <p className="sign-in-description">Sign in to DevCALT</p>
+          </div>
+          <div className="input-container">
+            <label htmlFor="email" className="input-label">Email</label>
+            <Input type="email" id="email" className="email-input" required />
+          </div>
+          <div className="input-container">
+            <label htmlFor="password" className="input-label">Password</label>
+            <Input type="password" id="password" className="password-input" required />
+          </div>
+          <div className="remember-me-container">
+            <input type="checkbox" id="remember-me" name="remember-me" className="checkbox" />
+            <label htmlFor="remember-me" className="checkbox-label">Remember me</label>
+          </div>
+          <button type="submit" className="submit-btn">Sign In</button>
+        </div>
 
-      <p>
-        Signup instead
-      </p>
-    </div>
+      </form>
+
+    </section>
   )
 }
 
